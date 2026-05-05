5567f625ee511190bb99f5c44f5b2d118952e391
sariel <3428958820@qq.com>
2026-05-05 20:55:40 +0800

    更改了page1的代码，实际上是重新覆盖重写了

--- page 1.py
+++ page 1.py
@@ -1,10 +1,36 @@
-import pandas as pd
-import numpy as np
-a=pd.Series([5,6,7.8,9,np.nan,299.3],dtype=float)
-#a.index #查看序列
-#print(a.values)#查看值
-a.index=list('abcdef')
-# print(a["a":"d"])
+"""
+多行注释只有第一行是灰色的
+（桀桀桀）
+"""S
+
+"""
+print(666)
+print("printf")
+print("c乱入")
+"""
+#学了一点c,又回来学python，发现忘的差不多了
+# 单行注释（加个空格就没有波浪线了）
+# 多行注释（在#号和内容之间加空格）
+"""
+print(4599)
+print(2740)
+"""
+
+#变量直接写，不需要写int money =50
+money=50
+print(money)
+cake=7
+money=money-cake
+print("钱包还有:",money)
+#假设每隔一小时，钱包的余额增加10元
+money=99
+print("下午一点，钱包余额为:",money)
+print("下午二点，钱包余额为:",money+10)
+money=money+10
+print("下午三点，钱包余额为:",money+10)
+
+
+
 
 
 
