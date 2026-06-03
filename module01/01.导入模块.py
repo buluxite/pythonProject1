90d1f7679812d956d374bd3b87ad33f42c2d1110
sariel <3428958820@qq.com>
2026-06-03 21:47:22 +0800

    导入模块功能

--- module01/01.导入模块.py
+++ module01/01.导入模块.py
@@ -3,13 +3,31 @@
 # for i in range(10):
 #     print(random.randint(1,10))
 # print("________________________________________________________________________")
-# import random as rd
+# import random as rd  导入
 # for i in range(20):
-#     print(rd.randint(1,50))
+#     print(rd.randint(1,50))   调用
 # 导入模块中的功能
 # from random import randint as rt
 # for i in range(10):
 #     print(rt(1,20))
-from modele1 import line
-line()
+
+# from modele1 import area
+# print(area(10))
+# import modele1
+# modele1.add()
+# from modele1 import *   # * 和all绑定
+# add()
+# print(area(10))
+# print(PI)
+# 导入模块
+# from module01 import modele1
+# modele1.line()
+# modele1.add()
+# 导入功能
+# from module01.modele1 import add,line
+# add()
+# line()
+from module01 import *  #  #  在包里调用，要在init里面定义
+modele1.add()
+
 
--- module01/modele1.py
+++ module01/modele1.py
@@ -1,9 +1,30 @@
+# --all--,用来指定 from ...import *里面的*，eg:
+# __all__=["line","add","area","PI"]
+
+
 def line():
     print("-" * 30)
 
 
 def add():
     print("+" * 30)
+
+def area(r):
+    a=3.14*r*r
+    return a
+def peri(r):
+    b=3.14*r*2
+    return b
+PI=3.1415926
+"""
+1,__name__ 的值是什么？
+当文件被直接运行时（比如你在终端敲 python script.py），Python 会把该文件的 __name__ 变量自动赋值为 "__main__"。
+当文件被其他文件导入时（比如 import script），Python 会把该文件的 __name__ 变量赋值为该模块的名字（即文件名 "script"）
+2,最经典的用法：if __name__ == '__main__'
+这段代码是专门留给当前文件自己测试用的
+"""
+if __name__=="__main__":
+    print(peri(10))
 
 
 
